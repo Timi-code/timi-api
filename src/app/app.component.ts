579acff3f1c120d0e8cb4a1507a4e14eb8bbaf04
汤成立 <tangchengli@cloudwalk.cn>
2020-10-30 17:44:17 +0800

    feat(): init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'timi-api';
+}
